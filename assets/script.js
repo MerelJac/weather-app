9387f72ac31a35bfa331f69fbf9ff3c925fffe00
Merel Jacobs <merel.burleigh@gmail.com>
2023-06-05 16:43:57 -0700

    added skeleton code and started functionality of submit button

---
+++ assets/script.js
@@ -0,0 +1,7 @@
+var submitBtn = document.querySelector('input[name="submit-button');
+var locationInput = document.querySelector('input[name="location-input"]');
+
+submitBtn.addEventListener("click", function() {
+    console.log(locationInput.value)
+})
+
