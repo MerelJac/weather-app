447905729bbf65a9fb336a618a792e2db1de5143
Merel Jacobs <merel.burleigh@gmail.com>
2023-06-07 14:09:24 -0700

    working on 5 day forcast and CSS styling for weather icons

--- assets/script.js
+++ assets/script.js
@@ -8,11 +8,10 @@ var apiKey = "17476851cd3efca9f4c619dbaa03a7d6";
 const proxyUrl = 'https://cors-anywhere.herokuapp.com/';
 
 function processData() {
-regenBtn.style.display = "flex";
 // get input from search bar
 var input = locationInput.value;
-// save to local storage
-localStorage.setItem("city", input);
+currentWeatherSection.style.display = "flex";
+regenBtn.style.display = "flex";
 // convert city name to lat lon
 fetch('http://api.openweathermap.org/geo/1.0/direct?q=' + input + '&limit=1&appid=' + apiKey)
 .then(function (locationResponse) {
@@ -36,7 +35,7 @@ fetch('http://api.openweathermap.org/geo/1.0/direct?q=' + input + '&limit=1&appi
                     const currentHumidity = JSON.stringify(currentData.main.humidity);
                     const currentWindSpeed = JSON.stringify(currentData.wind.speed);
                     // print to page
-                    currentWeatherSection.innerHTML += `<div class="current"><h2>${currentName}</h2><img src="${iconUrl}" alt="weatherIconCurrent"></div><div class="infoCurrent"><p>${currentDate}</p><p>Temp: ${currentTemp}</p><p>Humidity: ${currentHumidity}</p>Wind Speed: ${currentWindSpeed}</p></div>`;})
+                    currentWeatherSection.innerHTML += `<div class="current"><h2>${currentName}</h2><img id="weatherIcon" src="${iconUrl}" alt="weatherIconCurrent"></div><div class="infoCurrent"><p>${currentDate}</p><p>Temp: ${currentTemp}</p><p>Humidity: ${currentHumidity}</p>Wind Speed: ${currentWindSpeed}</p></div>`;})
         // needs the http or you will get a CORS error // 5 day weather
             fetch('http://api.openweathermap.org/data/2.5/forecast?lat='+ lat + '&lon='+ lon + '&limit=5&units=imperial&appid=' + apiKey)
                 .then(function (forcastResponse){
@@ -45,13 +44,14 @@ fetch('http://api.openweathermap.org/geo/1.0/direct?q=' + input + '&limit=1&appi
                 console.log(forcastData);
                 // add print data here
                 //add day value 
+                const forcastDate = forcastData.list[0].dt_txt;
                 const forcastIcon = forcastData.list[0].weather[0].icon;
                 const forcastIconUrl = 'https://openweathermap.org/img/wn/'+ forcastIcon +'.png';
                 const forcastTemp = JSON.stringify(forcastData.list[0].main.feels_like);
                 const forcastHumidity = JSON.stringify(forcastData.list[0].main.humidity);
                 const forcastWindSpeed = JSON.stringify(forcastData.list[0].wind.speed);
                 //print to page
-                futureWeatherSection.innerHTML += `<p>Days Away</p><img src="${forcastIconUrl}" alt="weatherIconCurrent"><p>Temp: ${forcastTemp}</p><p>Humidity: ${forcastHumidity}</p>Wind Speed: ${forcastWindSpeed}</p>`;})
+                futureWeatherSection.innerHTML += `<p>${forcastDate}</p><img id="weatherIcon" src="${forcastIconUrl}" alt="weatherIconCurrent"><p>Temp: ${forcastTemp}</p><p>Humidity: ${forcastHumidity}</p>Wind Speed: ${forcastWindSpeed}</p>`;})
                 }) };
 
 function localStorage() {
